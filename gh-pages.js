4c856beea45b4ea1ced15eca5cc272a5bf8e26f7
iinow <iinow@naver.com>
2021-09-22 07:42:51 +0900

    github action 배포 추가

--- gh-pages.js
+++ gh-pages.js
@@ -1,4 +1,19 @@
 const ghpages = require('gh-pages')
 
+const token = process.env.GIT_TOKEN
+if (!token) {
+  throw new Error('required "GIT_TOKEN" value')
+}
+
 ghpages.clean()
-ghpages.publish('public', {}, (err) => err)
+ghpages.publish(
+  'public',
+  {
+    repo: `https://iinow:${token}@github.com/iinow/iinow.github.io.git`,
+    branch: 'gh-pages',
+  },
+  (err) => {
+    console.log(err)
+    return err
+  }
+)
